857cb9fe044411ad35489fd8b89e70a5888417a4
Martino <martinobennguyen@gmail.com>
2025-06-23 19:09:16 +1000

    Added links to social icons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,10 +7,18 @@ const Header: React.FC = () => {
     return (
         <header className="portfolio-header">
             <div className="header-left">
-                <img src="/github.svg" alt="GitHub Logo" className="github-logo" />
-                <img src="/linkedin.svg" alt="LinkedIn Logo" className="linkedin-logo" />
-                <img src="/facebook.svg" alt="Facebook Logo" className="facebook-logo" />
-                <img src="/instagram.svg" alt="Instagram Logo" className="instagram-logo" />
+                <a href="https://github.com/Mar10787" target="_blank" rel="noopener noreferrer">
+                    <img src="/github.svg" alt="GitHub Logo" className="github-logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/martino-nguyen-173870335" target="_blank" rel="noopener noreferrer">
+                    <img src="/linkedin.svg" alt="LinkedIn Logo" className="linkedin-logo" />
+                </a>
+                <a href="https://www.facebook.com/martino.nguyen.948/" target="_blank" rel="noopener noreferrer">
+                    <img src="/facebook.svg" alt="Facebook Logo" className="facebook-logo" />
+                </a>
+                <a href="https://www.instagram.com/martino_n03" target="_blank" rel="noopener noreferrer">
+                    <img src="/instagram.svg" alt="Instagram Logo" className="instagram-logo" />
+                </a>
             </div>
             <div className="header-middle">
                 <h1>Martino Nguyen</h1>
